03c9b27536a1150bf8388d7698034298f2fa5d77
Sun Jiao <xzzzf.dsx@gmail.com>
2020-04-22 20:16:04 +0800

    Android Could not open the database in read/write mode.

--- osmunda/src/main/java/moe/sunjiao/osmunda/Osmunda.kt
+++ osmunda/src/main/java/moe/sunjiao/osmunda/Osmunda.kt
@@ -2,22 +2,23 @@ package moe.sunjiao.osmunda
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.util.Log
 import java.io.File
 import java.util.*
 
 class Osmunda(private val context: Context) {
 
     fun getDatabaseList() : Array<File>?{
-        return getOsmundaDir()
-            .listFiles { dir, name -> name.toLowerCase(Locale.ROOT).endsWith(".sqlite") }
+        return getDatabaseDir()
+            .listFiles { dir, name -> name.toLowerCase(Locale.ROOT).endsWith("-osmunda.sqlite") }
     }
 
     fun getDatabaseByName(name : String): SQLiteDatabase{
-        return context.openOrCreateDatabase(getOsmundaDir().absolutePath + "/" + name + "sqlite", 0, null)
+        return context.openOrCreateDatabase(getDatabaseDir().absolutePath + "/" + name + ".sqlite", 0, null)
     }
 
-    fun getOsmundaDir(): File{
-        return File(context.filesDir.absolutePath + "/osmunda")
+    fun getDatabaseDir(): File{
+        return File(context.filesDir.absolutePath.replace("files", "databases"))
     }
 
 }
