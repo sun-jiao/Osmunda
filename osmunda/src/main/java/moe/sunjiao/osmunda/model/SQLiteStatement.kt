85443bee23a84021cff409ea5dac3f60e9c3b422
Sun Jiao <xzzzf.dsx@gmail.com>
2020-04-24 22:20:56 +0800

    support multi language and delete suburb

--- osmunda/src/main/java/moe/sunjiao/osmunda/model/SQLiteStatement.kt
+++ osmunda/src/main/java/moe/sunjiao/osmunda/model/SQLiteStatement.kt
@@ -1,9 +1,15 @@
 package moe.sunjiao.osmunda.model
 
 class SQLiteStatement (
-    val state : String,
     val city : String,
     val county : String,
     val town : String,
     val suburb : String
-)
+){
+    init {
+
+    }
+
+    constructor():this("","","","")
+
+}
