2a0708c5f6e4188e56777378121e1106b15518f7
Sun Jiao <xzzzf.dsx@gmail.com>
2020-04-24 18:30:48 +0800

    get statement by country code

---
+++ osmunda/src/main/java/moe/sunjiao/osmunda/model/SQLiteStatement.kt
@@ -0,0 +1,9 @@
+package moe.sunjiao.osmunda.model
+
+class SQLiteStatement (
+    val state : String,
+    val city : String,
+    val county : String,
+    val town : String,
+    val suburb : String
+)
