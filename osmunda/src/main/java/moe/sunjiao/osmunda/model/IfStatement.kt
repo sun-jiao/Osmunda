baf454547aedd70be3fe699352a29560634904b2
Sun Jiao <xzzzf.dsx@gmail.com>
2020-04-24 23:29:19 +0800

    support multi language

--- osmunda/src/main/java/moe/sunjiao/osmunda/model/IfStatement.kt
+++ osmunda/src/main/java/moe/sunjiao/osmunda/model/IfStatement.kt
@@ -2,53 +2,26 @@ package moe.sunjiao.osmunda.model
 
 import java.util.*
 
-class IfStatement (
-    _city            : (current: String, key: String, value: String) -> Boolean,
-    _county          : (current: String, key: String, value: String) -> Boolean,
-    _town            : (current: String, key: String, value: String) -> Boolean,
-    _neighbourhood   : (current: String, key: String, value: String) -> Boolean
-){
+class IfStatement (){
 
-    var city            : (current: String, key: String, value: String) -> Boolean = {_, _, _ -> true}
-        private set(value) {
-            field = value
-        }
-    var county          : (current: String, key: String, value: String) -> Boolean = {_, _, _ -> true}
-        private set(value) {
-            field = value
-        }
-    var town            : (current: String, key: String, value: String) -> Boolean = {_, _, _ -> true}
-        private set(value) {
-            field = value
-        }
-    var neighbourhood   : (current: String, key: String, value: String) -> Boolean = {_, _, _ -> true}
-        private set(value) {
-            field = value
-        }
+    var city            : (current: String, key: String, value: String) -> Boolean = { _, key, _ -> key == "city" || key.endsWith(":city") }
+        private set
+    var county          : (current: String, key: String, value: String) -> Boolean = { _, key, _ -> key == "county_name" || key == "district_name" || key.endsWith("county") || key.endsWith("district")}
+        private set
+    var town            : (current: String, key: String, value: String) -> Boolean = { _,key,_ -> key.endsWith("town")}
+        private set
+    var neighbourhood   : (current: String, key: String, value: String) -> Boolean = { current, key, value -> "operator" == key || (current == "" && (key.endsWith("neighbourhood") || (key.endsWith("village") )))}
+        private set
 
-    init{
-        city = _city
-        county = _county
-        town = _town
-        neighbourhood = _neighbourhood
-    }
-
-    constructor() : this(
-        { _, key, _ -> key == "city" || key.endsWith(":city") },
-        { _, key, _ -> key == "county_name" || key == "district_name" || key.endsWith("county") || key.endsWith("district")},
-        { _,key,_ -> key.endsWith("town")},
-        { current, key, value -> "operator" == key || (current == "" && (key.endsWith("neighbourhood") || (key.endsWith("village") )))}
-    )
-
-
+    init{ }
 
     constructor(locale: Locale?): this(){
         when (locale) {
             Locale.SIMPLIFIED_CHINESE, Locale.CHINA, Locale.CHINESE, Locale.PRC -> this(
-                { _, key, value -> key.startsWith("addr:city") && value.endsWith("市") },
-                { _, key, value -> key == "county_name" || key == "district_name" || (key.startsWith("addr:") && (value.endsWith("县") || value.endsWith("区")))},
-                { _, key, value -> key.startsWith("addr:") && (value.endsWith("乡") || value.endsWith("镇") || value.endsWith("街道") || value.endsWith("街道办事处")) },
-                { current, key, value -> "operator" == key || (current == "" && (key.endsWith("neighbourhood") || key.endsWith("village") || value.endsWith("村")))}
+                { _, key, value -> key == "addr:city" && value.endsWith("市") },
+                { _, key, value -> key == "county_name" || key == "district_name" || ((key.startsWith("addr:") && (value.endsWith("县")) || value.endsWith("区")))},
+                { _, key, value -> key.startsWith("addr:") && ((value.endsWith("乡") || value.endsWith("镇") || value.endsWith("街道") || value.endsWith("街道办事处"))) },
+                { current, key, value -> "operator" == key || (current == "" && (key.endsWith("neighbourhood") || key.endsWith("village") || (key.startsWith("addr:") && (value.endsWith("村") || value.endsWith("庄") ))))}
             )
         }
     }
--- osmunda/src/main/java/moe/sunjiao/osmunda/model/SQLiteStatement.kt
+++ osmunda/src/main/java/moe/sunjiao/osmunda/model/SQLiteStatement.kt
@@ -1,15 +1,54 @@
 package moe.sunjiao.osmunda.model
 
-class SQLiteStatement (
-    val city : String,
-    val county : String,
-    val town : String,
-    val suburb : String
-){
-    init {
+import java.util.*
 
+class SQLiteStatement (){
+    var city : String = " k like '%city' "
+        private set
+    var county_way : String = " k like '%county' or k like '%district' "
+        private set
+    var county_node : String = " k = 'place' and ( v = 'county' or v = 'district' ) "
+        private set
+    var town_way : String = " k like '%town' "
+        private set
+    var town_node : String = " (k = 'place' and v = 'town') "
+        private set
+    var neighbourhood_way : String = " k like '%neighbourhood' or k like '%village' or k = 'operator' "
+        private set
+    var neighbourhood_node : String = " (k = 'place' and (v = 'neighbourhood' or v = 'village')) "
+        private set
+
+    init { }
+
+    constructor(locale: Locale?) : this(){
+        when (locale) {
+            Locale.SIMPLIFIED_CHINESE, Locale.CHINA, Locale.CHINESE, Locale.PRC -> this(
+                " k like '%city' and v like '%市' ",
+                " k like '%county' or k like '%district' or ( k like 'addr:%' and ( v like '%县' or v like '%区' )) ",
+                " ( k = 'place' and ( v = 'county' or v = 'district' )) or ( k = 'china_class' and ( v = 'xian' )) or ( k = 'name' and ( v like '%县' or v like '%区' )) ",
+                " k like '%town' or ( k like 'addr:%' and ( v like '%乡' or v like '%镇' or v like '%街道' or v like '%街道办事处' )) ",
+                " (k = 'place' and v = 'town') or (k = 'china_class' and ( v = 'zhen' or v = 'xiang' or v = 'jiedao'))  or ( k = 'name' and ( v like '%乡' or v like '%镇' or v like '%街道' or v like '%街道办事处'  )) ",
+                " k like '%neighbourhood' or k like '%village' or k = 'operator' ",
+                " ((k = 'place' or k = 'china_class') and (v = 'neighbourhood' or v = 'village')) or ( k = 'name' and ( v like '%村' or v like '%庄' )) "
+            )
+        }
     }
 
-    constructor():this("","","","")
-
+    private operator fun invoke(
+        _city : String,
+        _county_way : String,
+        _county_node : String,
+        _town_way : String,
+        _town_node : String,
+        _neighbourhood_way : String,
+        _neighbourhood_node : String
+    ) {
+        city = _city
+        county_way = _county_way
+        county_node = _county_node
+        town_way = _town_way
+        town_node = _town_node
+        neighbourhood_way = _neighbourhood_way
+        neighbourhood_node = _neighbourhood_node
+    }
 }
